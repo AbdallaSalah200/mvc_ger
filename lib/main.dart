c4892f7998c065ddaaa3a0e306d68469cd240b18
AbdallaSalah200 <142856930+AbdallaSalah200@users.noreply.github.com>
2023-09-17 16:02:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:mvc_get/views/home.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  GetMaterialApp(
+      home: Home(),
+    );
+  }
+}
